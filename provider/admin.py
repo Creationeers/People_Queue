5f62c41bc6c9d54fde8eea15c82db7b73b301c50
enzohilzinger <en.hzg53@gmail.com>
2019-10-25 21:57:05 +0200

    added table to admin

--- provider/admin.py
+++ provider/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Profile, Venue, Bonus, Reservation
+from .models import Profile, Venue, Bonus, Reservation, Occupation_Past_Data
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Venue)
 admin.site.register(Bonus)
 admin.site.register(Reservation)
+admin.site.register(Occupation_Past_Data)
