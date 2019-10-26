50e5a5f23f9719a042fa91df49a860ac19712cc4
enzohilzinger <en.hzg53@gmail.com>
2019-10-26 10:17:04 +0200

    transaction rollbacks

--- provider/util/messages.py
+++ provider/util/messages.py
@@ -1,2 +1,3 @@
 USER_CREATED = "User has been successfully created"
 USER_NAME_EXISTS = "Username is already taken"
+MISSING_FIELD = "Required Fields are missing"
--- provider/views/admin_views.py
+++ provider/views/admin_views.py
@@ -1,12 +1,14 @@
 from datetime import timedelta
+from django.core.exceptions import ValidationError
 from django.contrib.auth.models import User
 from django.db import IntegrityError, transaction
 from rest_framework import exceptions, permissions, generics
 from rest_framework.views import APIView, status
 from rest_framework.response import Response
 from decouple import config
-from ..util.messages import USER_CREATED, USER_NAME_EXISTS
+from ..util.messages import USER_CREATED, USER_NAME_EXISTS, MISSING_FIELD
 from ..util.builders import ResponseBuilder
+from ..models import Profile
 
 ResponseBuilder = ResponseBuilder()
 import logging
@@ -19,14 +21,24 @@ class RegisterUserView(APIView):
 
     def post(self, request, *args, **kwargs):
         try:
-            user = User.objects.create_user(
-                username=request.data['username'], password=request.data['password'], email=request.data['email'])
-            logger.info('User {} Created Successfully'.format(user.username))
-            return ResponseBuilder.get_response(message=USER_CREATED, status=status.HTTP_201_CREATED)
+            sid = transaction.savepoint()
+            with transaction.atomic():
+                user = User.objects.create_user(
+                    username=request.data['username'], password=request.data['password'], email=request.data['email'])
+                profile = Profile.objects.get(user=user)
+                profile.ad_consent_available = request.data['is_commercial_user'].lower().capitalize()
+                profile.is_commercial_user = request.data['ad_consent_available'].lower().capitalize()
+                profile.save()
+                logger.info('User {} with Profile ID {} Created Successfully'.format(user.username, user.profile.id))
+                return ResponseBuilder.get_response(message=USER_CREATED, status=status.HTTP_201_CREATED)
         except IntegrityError as e:
-            transaction.rollback()
+            transaction.savepoint_rollback(sid)
             logger.error('User Creation Failed: {}'.format(e.args[0]))
             if(UNIQUE_CONSTRAINT in e.args[0].lower()):
                 return ResponseBuilder.get_response(message=USER_NAME_EXISTS, status=status.HTTP_409_CONFLICT)
             return ResponseBuilder.get_response(message=e.args, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        except ValidationError as e:
+            transaction.savepoint_rollback(sid)
+            logger.error('Transactional Error: User Creation Failed: {}'.format(e.args[0]))
+            return ResponseBuilder.get_response(message=MISSING_FIELD, status=status.HTTP_406_NOT_ACCEPTABLE)
 
