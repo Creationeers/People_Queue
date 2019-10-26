3c3798559cb3f7e564e05de0a4dad529b5e352b6
enzohilzinger <en.hzg53@gmail.com>
2019-10-26 09:50:31 +0200

    added logging

--- provider/views/admin_views.py
+++ provider/views/admin_views.py
@@ -2,17 +2,18 @@ from datetime import timedelta
 from django.contrib.auth.models import User
 from django.db import IntegrityError, transaction
 from rest_framework import exceptions, permissions, generics
-from rest_framework.views import status
+from rest_framework.views import APIView, status
 from rest_framework.response import Response
 from decouple import config
 from ..util.messages import USER_CREATED, USER_NAME_EXISTS
 from ..util.builders import ResponseBuilder
 
 ResponseBuilder = ResponseBuilder()
-
+import logging
+logger = logging.getLogger('testlogger')
 UNIQUE_CONSTRAINT = 'unique constraint'
 
-class RegisterUserView(generics.ListCreateAPIView):
+class RegisterUserView(APIView):
     permission_classes = (permissions.AllowAny,)
     authentication_classes = ()
 
@@ -23,6 +24,7 @@ class RegisterUserView(generics.ListCreateAPIView):
             return ResponseBuilder.get_response(message=USER_CREATED, status=status.HTTP_201_CREATED)
         except IntegrityError as e:
             transaction.rollback()
+            logger.error('User Creation Failed: {}'.format(e.args[0]))
             if(UNIQUE_CONSTRAINT in e.args[0].lower()):
                 return ResponseBuilder.get_response(message=USER_NAME_EXISTS, status=status.HTTP_409_CONFLICT)
             return ResponseBuilder.get_response(message=e.args, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
